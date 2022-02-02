51b715bc6cb928353ce3a651e367482fbc1828d5
Tiziano Basile <tiz.basile@gmail.com>
2022-02-02 08:12:25 +0100

    Edited POJO to return data with different names than the remote API

--- src/main/java/com/basteez/jackson_annotation_poc/pojo/Civilization.java
+++ src/main/java/com/basteez/jackson_annotation_poc/pojo/Civilization.java
@@ -1,32 +1,113 @@
 package com.basteez.jackson_annotation_poc.pojo;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
 
 import java.util.List;
 
 public class Civilization {
-    @JsonProperty("id")
+    @JsonProperty("Id")
     public Integer id;
 
-    @JsonProperty("name")
+    @JsonProperty("Name")
     public String name;
 
-    @JsonProperty("expansion")
+    @JsonProperty("Expansion")
     public String expansion;
 
-    @JsonProperty("army_type")
+    @JsonProperty("ArmyType")
     public String armyType;
 
-    @JsonProperty("unique_unit")
+    @JsonProperty("UniqueUnit")
     public List<String> uniqueUnit;
 
-    @JsonProperty("unique_tech")
+    @JsonProperty("UniqueTech")
     public List<String> uniqueTech;
 
-    @JsonProperty("team_bonus")
+    @JsonProperty("TeamBonus")
     public String teamBonus;
 
-    @JsonProperty("civilization_bonus")
+    @JsonProperty("CivilizationBonus")
     public List<String> civilizationBonus;
 
+    @JsonGetter("Id")
+    public Integer getId() {
+        return id;
+    }
+
+    @JsonSetter("id")
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @JsonGetter("Name")
+    public String getName() {
+        return name;
+    }
+
+    @JsonSetter("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonGetter("Expansion")
+    public String getExpansion() {
+        return expansion;
+    }
+
+    @JsonSetter("expansion")
+    public void setExpansion(String expansion) {
+        this.expansion = expansion;
+    }
+
+    @JsonGetter("ArmyType")
+    public String getArmyType() {
+        return armyType;
+    }
+
+    @JsonSetter("army_type")
+    public void setArmyType(String armyType) {
+        this.armyType = armyType;
+    }
+
+    @JsonGetter("UniqueUnit")
+    public List<String> getUniqueUnit() {
+        return uniqueUnit;
+    }
+
+    @JsonSetter("unique_unit")
+    public void setUniqueUnit(List<String> uniqueUnit) {
+        this.uniqueUnit = uniqueUnit;
+    }
+
+    @JsonGetter("UniqueTech")
+    public List<String> getUniqueTech() {
+        return uniqueTech;
+    }
+
+    @JsonSetter("unique_tech")
+    public void setUniqueTech(List<String> uniqueTech) {
+        this.uniqueTech = uniqueTech;
+    }
+
+    @JsonGetter("TeamBonus")
+    public String getTeamBonus() {
+        return teamBonus;
+    }
+
+    @JsonSetter("team_bonus")
+    public void setTeamBonus(String teamBonus) {
+        this.teamBonus = teamBonus;
+    }
+
+    @JsonGetter("CivilizationBonus")
+    public List<String> getCivilizationBonus() {
+        return civilizationBonus;
+    }
+
+    @JsonSetter("civilization_bonus")
+    public void setCivilizationBonus(List<String> civilizationBonus) {
+        this.civilizationBonus = civilizationBonus;
+    }
 }
